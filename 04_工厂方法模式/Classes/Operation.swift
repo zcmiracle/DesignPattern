4208263edd9a3b701170e73aa5bb7eeb56abee54
Fearless <489368600@qq.com>
2021-08-19 17:35:11 +0800

    工厂方法模式

---
+++ 04_工厂方法模式/Classes/Operation.swift
@@ -0,0 +1,17 @@
+//
+//  Operation.swift
+//  04_工厂方法模式
+//
+//  Created by XFB on 2021/8/19.
+//
+
+import Foundation
+
+class Operation {
+    var numberA: Double = 0.0
+    var numberB: Double = 0.0
+
+    func GetResult() -> Double {
+        return 0.0
+    }
+}
---
+++ 04_工厂方法模式/Classes/OperationMul.swift
@@ -0,0 +1,16 @@
+//
+//  OperationMul.swift
+//  03_工厂方法模式
+//
+//  Created by XFB on 2021/8/19.
+//
+
+import Foundation
+
+class OperationMul: Operation {
+    
+    override func GetResult() -> Double {
+        numberA * numberB
+    }
+    
+}
