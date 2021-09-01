0a443e4f927cc3e1af8e5ac7a5543a813f981787
Fearless <489368600@qq.com>
2021-09-01 10:28:19 +0800

    工厂方法

--- 06_工厂方法/Classes/ComputerFactory.swift
+++ 06_工厂方法/Classes/ComputerFactory.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 // 其实可以定制成协议，MacComputerFactory和HWComputerFactory 实现协议即可
+// 协议类似于定义一组接口的含义
 // 如果需要新增 惠普电脑，则新建一个惠普电脑工厂，然后实现协议即可
 protocol ComputerFactory {
     func createComputer() -> Computer
@@ -18,4 +19,5 @@ protocol ComputerFactory {
 //    func createComputer() -> Computer? {
 //        return nil
 //    }
+//
 //}
